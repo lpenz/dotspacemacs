570ade2c9668d64db8f81db62303cc08e071a64d
Leandro Lisboa Penz <lpenz@lpenz.org>
2016-12-15 22:34:38 +0000

    Use sanityinc-solarized-dark as default theme

--- init.el
+++ init.el
@@ -138,7 +138,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(misterioso
+   dotspacemacs-themes '(sanityinc-solarized-dark
+                         misterioso
                          spacemacs-dark
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.

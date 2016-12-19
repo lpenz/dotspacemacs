afdaa2fe9c693a1ea985203ba8e805d997e19bff
Leandro Lisboa Penz <lpenz@lpenz.org>
2016-12-19 00:21:49 +0000

    Turn on auto-save

--- init.el
+++ init.el
@@ -324,6 +324,7 @@ you should place your code here."
   (define-key evil-normal-state-map (kbd "<f2>") 'save-buffer)
   (define-key evil-normal-state-map (kbd "<f10>") 'projectile-compile-project)
   ; (define-key evil-normal-state-map (kbd "C-C C-C") 'save-buffers-kill-terminal); magic commit shortcut!
+  (setq dotspacemacs-auto-save-file-location 'original)
   (setq powerline-default-separator 'utf-8)
   (setq python-shell-interpreter "python")
   )

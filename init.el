d350b42e16bf1fe560a17fcc0eaf2560485a5310
Leandro Lisboa Penz <lpenz@lpenz.org>
2016-12-27 20:27:00 +0000

    Add ranger layer (SPC a r)

--- init.el
+++ init.el
@@ -66,6 +66,7 @@ values."
      ansible
      semantic
      perforce
+     (ranger :variables ranger-show-preview t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

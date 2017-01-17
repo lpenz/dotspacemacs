224b6b8d59ac9d909108fc4b3734728cf39fa33d
Leandro Lisboa Penz <lpenz@arista.com>
2017-01-17 16:56:47 +0000

    Install "go" layer

--- init.el
+++ init.el
@@ -71,6 +71,7 @@ values."
      perforce
      javascript
      (ranger :variables ranger-show-preview t)
+     go
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

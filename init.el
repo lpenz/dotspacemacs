7d1db63b38b48353b214a541c81a5515a11c75cf
Leandro Lisboa Penz <lpenz@arista.com>
2017-05-23 11:44:45 +0100

    imenu-list layer

--- init.el
+++ init.el
@@ -75,6 +75,7 @@ values."
      csv
      nixos
      docker
+     imenu-list
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

7087b945ea2e44b21876e81cde65e7229fd3a477
Leandro Lisboa Penz <lpenz@arista.com>
2017-06-20 10:21:20 +0100

    Use Nerd Font; enable unicode in mode line

--- init.el
+++ init.el
@@ -163,7 +163,8 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '(
-                               "DejaVuSansMono:size=15:antialias=true:autohint=true"
+                               "DejaVuSansMonoForPowerline Nerd Font:size=15:antialias=true:autohint=true"
+                               ; "DejaVuSansMono:size=15:antialias=true:autohint=true"
                                ; "SourceCodePro:size=13:weight=normal:width=normal"
                                :powerline-scale 1.1
                                )
@@ -271,7 +272,7 @@ values."
    ;; If non nil show the color guide hint for transient state keys. (default t)
    dotspacemacs-show-transient-state-color-guide t
    ;; If non nil unicode symbols are displayed in the mode line. (default t)
-   dotspacemacs-mode-line-unicode-symbols nil
+   dotspacemacs-mode-line-unicode-symbols t
    ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
    ;; scrolling overrides the default behavior of Emacs which recenters point
    ;; when it reaches the top or bottom of the screen. (default t)
